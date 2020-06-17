30da93056a306efeafe41b34417c5c5ee87a929f
HP Dietz <hp@jandh.org>
2020-06-17 12:15:16 +0200

    Publish useAnimationFrame in main entry.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import _useAnimationFrame from './useAnimationFrame';
 import _useDebounce from './useDebounce';
 
+export const useAnimationFrame = _useAnimationFrame;
 export const useDebounce = _useDebounce;
 
 export default {
+  useAnimationFrame,
   useDebounce
 };
